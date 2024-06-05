a0947245265e3687872fb838cf4bf6afd64ffd34
YOUR NAME <mkk1398@gmail.com>
2024-06-05 16:58:48 -0700

    example of using tkinter to display plot run from terminal instead of IDE

---
+++ plottest.py
@@ -0,0 +1,12 @@
+import matplotlib
+import matplotlib.pyplot as plt
+import numpy as np
+
+matplotlib.use("TkAgg")
+
+x = [1,2,3,4,5,6,7,8,9]
+y = [2,4,6,7,8,3,4,5,6]
+
+plt.plot(x, y)
+plt.show()
+
