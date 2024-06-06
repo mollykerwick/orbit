fd5669c7594b40dd555c33994db1f926366f2976
YOUR NAME <mkk1398@gmail.com>
2024-06-05 17:00:06 -0700

    included link for instructions on tkinter

--- plottest.py
+++ plottest.py
@@ -2,7 +2,11 @@ import matplotlib
 import matplotlib.pyplot as plt
 import numpy as np
 
+
 matplotlib.use("TkAgg")
+# added to display plot initialized from terminal using tkinter
+# from: https://github.com/matplotlib/matplotlib/issues/13414
+
 
 x = [1,2,3,4,5,6,7,8,9]
 y = [2,4,6,7,8,3,4,5,6]
