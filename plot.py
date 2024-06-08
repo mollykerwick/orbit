73d69a075fd2279c7b4e79a8d24ec7494bc97393
YOUR NAME <mkk1398@gmail.com>
2024-06-07 21:39:29 -0700

    changed orbit coordinate file names

--- plot.py
+++ plot.py
@@ -1,10 +1,16 @@
-import matplotlib.pyplot as plt
+import matplotlib
+matplotlib.use("TkAgg")
+
 # import numpy as np
 import pandas as pd
+import matplotlib.pyplot as plt
+
+
+
 
 if __name__ == '__main__':
-    df = pd.read_csv('hw4_orbit_Earth.txt', sep=' ', names=['x','y'])
-    df2 = pd.read_csv('hw4_orbit_Jupiter.txt', sep=' ', names=['x','y'])
+    df = pd.read_csv('orbit_Earth.txt', sep=' ', names=['x','y'])
+    df2 = pd.read_csv('orbit_Jupiter.txt', sep=' ', names=['x','y'])
     
     ax = df.plot(x=df.columns[0],y=df.columns[1],kind='line')
     ax = df2.plot(x=df2.columns[0],y=df2.columns[1],kind='line',ax=ax)
